1a7b309af7b40e6a78c2dec57979aee727af5102
Konrad Kęska <keska.konrad@gmail.com>
2020-11-09 23:15:57 +0100

    add constants

---
+++ src/constants.ts
@@ -0,0 +1,5 @@
+const RECORD_LIMIT: number = 50;
+const STRANGER_LABEL = "Want a quick guide? 🚍";
+const STRANGER_BTN_LABEL = "Yeah!";
+
+export { RECORD_LIMIT, STRANGER_LABEL, STRANGER_BTN_LABEL };

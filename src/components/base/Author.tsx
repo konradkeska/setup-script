603a9eb8073a083f440e6dca6527c3eff1f35488
Konrad Kęska <keska.konrad@gmail.com>
2020-11-11 02:40:34 +0100

    refactor base components, make them pure

---
+++ src/components/base/Author.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+import { Link } from "./Link";
+import { Span } from "./Span";
+
+const Author = React.memo(() => (
+  <Span>
+    Made with ♥️ by&nbsp;
+    <Link
+      href="https://github.com/konradkeska"
+      rel="noopener noreferer"
+      target="_blank"
+    >
+      Konrad Kęska
+    </Link>
+    &nbsp;in 2020.
+  </Span>
+));
+
+export default Author;

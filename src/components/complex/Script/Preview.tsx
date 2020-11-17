eb93bc8c8e08ca66e8a974150ae77e9b23b23a85
Konrad Kęska <keska.konrad@gmail.com>
2020-11-17 20:49:09 +0100

    add script preview scroll

--- src/components/complex/Script/Preview.tsx
+++ src/components/complex/Script/Preview.tsx
@@ -13,6 +13,7 @@ export const Preview = styled(Code)`
   flex-direction: column;
   align-items: flex-start;
   justify-content: flex-start;
+  overflow-y: auto;
 
   &:hover {
     user-select: unset;

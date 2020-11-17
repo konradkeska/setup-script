cdbba65688e19ed55b3993ed45e7332e3bb64cc2
Konrad Kęska <keska.konrad@gmail.com>
2020-11-17 20:43:08 +0100

    add useSearch hook

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,5 +1,6 @@
 export { useHotkeys } from "./useHotkeys";
 export { useList } from "./useList";
+export { useSearch } from "./useSearch";
 export { useSides } from "./useSides";
 export { useTheme } from "./useTheme";
 export { useTour } from "./useTour";
---
+++ src/hooks/useSearch.ts
@@ -0,0 +1,34 @@
+import { useState, useMemo } from "react";
+
+import { Soft } from "../types";
+import { includesQuery, sort } from "../utils/helpers";
+
+type Props = {
+  casks: Soft[];
+  formulas: Soft[];
+};
+
+export function useSearch({ casks, formulas }: Props) {
+  const [query, setQuery] = useState<string>("");
+  const hasValidQuery = useMemo(() => query?.length > 1, [query]);
+
+  const caskResults = useMemo(
+    () => (hasValidQuery ? includesQuery(query, casks) : []),
+    [hasValidQuery, query, casks]
+  );
+
+  const formulaeResults = useMemo(
+    () => (hasValidQuery ? includesQuery(query, formulas) : []),
+    [hasValidQuery, query, formulas]
+  );
+
+  const sortedResults = useMemo(
+    () =>
+      sort([...caskResults, ...formulaeResults], (a, b) =>
+        a.name.localeCompare(b.name, "en", { sensitivity: "base" })
+      ),
+    [caskResults, formulaeResults]
+  );
+
+  return { query, setQuery, sortedResults };
+}

e8b279e27279792fc05f3a5e3977f2c0c4daf9ea
Konrad Kęska <keska.konrad@gmail.com>
2020-11-22 17:25:55 +0100

    add drag dots icon

--- src/components/global/Sprites.tsx
+++ src/components/global/Sprites.tsx
@@ -15,5 +15,12 @@ export const Sprites = React.memo(() => (
       <title>arrow-right</title>
       <polygon points="79.093,0 48.907,30.187 146.72,128 48.907,225.813 79.093,256 207.093,128" />
     </symbol>
+    <symbol id="drag-dots" viewBox="0 0 16 16">
+      <title>drag-dots</title>
+      <path
+        fill-rule="evenodd"
+        d="M10 13a1 1 0 100-2 1 1 0 000 2zm-4 0a1 1 0 100-2 1 1 0 000 2zm1-5a1 1 0 11-2 0 1 1 0 012 0zm3 1a1 1 0 100-2 1 1 0 000 2zm1-5a1 1 0 11-2 0 1 1 0 012 0zM6 5a1 1 0 100-2 1 1 0 000 2z"
+      ></path>
+    </symbol>
   </svg>
 ));

8717af94579ac7546b062efbca61678bbf3cf604
Konrad Kęska <keska.konrad@gmail.com>
2020-11-28 12:15:13 +0100

    add modifier to useHotkeys

--- src/hooks/useHotkeys.ts
+++ src/hooks/useHotkeys.ts
@@ -1,15 +1,24 @@
 import { useCallback, useEffect, useMemo } from "react";
 
+type Hotkeys = {
+  getHotkeys: () => Record<KeyboardEvent["code"], () => void>;
+  modifier?: "ctrlKey" | "shiftKey" | "shiftKey";
+  event?: "keydown" | "keypress" | "keyup";
+};
+
 export function useHotkeys(
-  getHotkeys: () => Record<string, () => void>,
-  dependencies: unknown[],
-  event: "keydown" | "keypress" | "keyup" = "keydown"
-): void {
+  { getHotkeys, modifier, event = "keydown" }: Hotkeys,
+  dependencies: unknown[]
+) {
   const hotkeys = useMemo(getHotkeys, [getHotkeys, ...dependencies]);
 
   const onCallback = useCallback(
-    ({ code }: KeyboardEvent) => hotkeys[code]?.(),
-    [hotkeys]
+    (e: KeyboardEvent) => {
+      return modifier
+        ? e[modifier] && hotkeys[e.code]?.()
+        : hotkeys[e.code]?.();
+    },
+    [hotkeys, modifier]
   );
 
   useEffect(() => {

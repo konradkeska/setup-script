116be145d34d7458ac6e13b2373c003a3b0456df
Konrad Kęska <keska.konrad@gmail.com>
2020-11-30 02:17:33 +0100

    make script less messy

--- src/components/organisms/Script/Script.tsx
+++ src/components/organisms/Script/Script.tsx
@@ -17,38 +17,35 @@ export const Script = React.memo(({ casks, formulas }: Props) => (
     <Preview>
       #!/usr/bin/env bash
       <br />
-      <br /> # Ask for the administrator password upfront
+      <br /> # Ask for the admin permission upfront
       <br />
       sudo -v
       <br />
-      <br /> # Update existing `sudo` permission until process is finished
+      <br /> # Update existing permission until process is finished
       <br />
       while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2`
       {">"}`/dev/null &
       <br />
       <br /> # Install brew
       <br />
-      {
-        '/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"'
-      }
+      /bin/bash -c "$(curl -fsSL
+      https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
       <br />
       <br /> # {APP.FORMULAS_PANEL_HEADING} {APP.FORMULAS_PANEL_DESCRIPTION}
       <br />
-      brew install (<br />
+      brew install
+      <br />
       {formulas.map(({ name }, index) => (
         <BrewRecord key={index} token={name} />
       ))}
-      ) &
-      <br />
       <br /># {APP.CASKS_PANEL_HEADING} {APP.CASKS_PANEL_DESCRIPTION}
       <br />
-      brew cask install (<br />
+      brew cask install
+      <br />
       {casks.map(({ token, name }, index) => (
         <BrewRecord key={index} token={token || name} />
       ))}
-      ) &
-      <br />
-      <br /># Reboot system in 1 minute
+      <br /># Reboot system in 60 seconds
       <br />
       shutdown -r +1
     </Preview>
@@ -61,7 +58,7 @@ type BrewRecordProps = {
 
 const BrewRecord = React.memo(({ token }: BrewRecordProps) => (
   <span>
-    &nbsp;&nbsp;{token}
+    &nbsp;{token}
     <br />
   </span>
 ));

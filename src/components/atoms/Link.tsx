bacbad59427b7694a0cdb576851d83c85fe506af
Konrad Kęska <keska.konrad@gmail.com>
2020-12-20 16:15:24 +0100

    add toasts to key services

--- src/components/atoms/Link.tsx
+++ src/components/atoms/Link.tsx
@@ -4,16 +4,22 @@ import Sc from "styled-components";
 type Props = {
   children: React.ReactNode;
   href: string;
+  withoutMargin?: boolean;
 };
 
-export const Link = ({ children, href }: Props) => (
-  <Anchor href={href} target="_blank" rel="noopener noreferrer">
+export const Link = ({ children, href, withoutMargin = false }: Props) => (
+  <Anchor
+    href={href}
+    target="_blank"
+    rel="noopener noreferrer"
+    withoutMargin={withoutMargin}
+  >
     {children}
   </Anchor>
 );
 
-const Anchor = Sc.a`
-  margin: 0 0.33rem;
+const Anchor = Sc.a<Pick<Props, "withoutMargin">>`
+  margin: ${({ withoutMargin }) => (withoutMargin ? "none" : "0 0.33rem")};
   font-weight: 600;
   border: 1px solid transparent;
   &:hover {
